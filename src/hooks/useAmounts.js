3700e1f21d2d94826066dc0fa6fdd04825fb4dea
Christiaan Hemerik <Christiaan.Hemerik@thelearningnetwork.nl>
2022-02-06 14:01:32 +0100

    Retrieve stack type data from JSON

--- src/hooks/useAmounts.js
+++ src/hooks/useAmounts.js
@@ -1,31 +1,5 @@
 import { useEffect, useState } from 'react';
-
-const stackTypes = {
-    largeBoxes: {
-        label: 'Large boxes',
-        maxAmount: 6,
-        maxHeight: 200,
-        width: 34,
-        length: 49,
-        height: 34,
-    },
-    smallBoxes: {
-        label: 'Small boxes',
-        maxAmount: 8,
-        maxHeight: 200,
-        width: 33,
-        length: 49,
-        height: 25,
-    },
-    crates: {
-        label: 'Crates',
-        maxAmount: 5,
-        maxHeight: 190,
-        width: 41,
-        length: 78,
-        height: 38,
-    },
-};
+import stackTypes from '../stack-types.json';
 
 const defaultState = Object.keys(stackTypes)
     .reduce((previous, current) => ({
